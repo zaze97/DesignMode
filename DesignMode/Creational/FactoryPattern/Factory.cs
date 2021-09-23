98a37ce37bee99c7dc38c0e98ec3ef4daff065ad
张铮 <834207172@qq.com>
2021-09-23 10:38:11 +0800

    工厂模式

---
+++ DesignMode/Creational/FactoryPattern/Factory.cs
@@ -0,0 +1,78 @@
+﻿using System;
+///工厂模式
+namespace DesignMode.Creational.FactoryPattern
+{
+    
+    public interface IFactory
+    {
+        double _numberA { get; set; }
+        double _numberB { get; set; }
+        double GetResult();
+    }
+
+    abstract class Factory : IFactory
+    {
+        public double _numberA { get; set; }
+        public double _numberB { get; set; }
+        public abstract double GetResult();
+    }
+
+    class FactoryAdd : Factory, IFactory
+    {
+        public override double GetResult()
+        {
+            return _numberA + _numberB;
+        }
+    }
+
+    class FactorySub : Factory, IFactory
+    {
+        public override double GetResult()
+        {
+            return _numberA - _numberB;
+        }
+    }
+
+    class FactoryMul : Factory, IFactory
+    {
+        public override double GetResult()
+        {
+            return _numberA * _numberB;
+        }
+    }
+
+    class FactoryDiv : Factory, IFactory
+    {
+        public override double GetResult()
+        {
+            return _numberA / _numberB;
+        }
+    }
+
+    class  Operation
+    {
+        public IFactory OperationSymbol(string symbol)
+        {
+            switch (symbol)
+            {
+                case "+":
+                    return new FactoryAdd();
+                    break;
+                case "-":
+                    return new FactorySub();
+                    break;
+                case "*":
+                    return new FactoryMul();
+                    break;
+                case "/":
+                    return new FactoryDiv();
+                    break;
+                default:
+                    Console.WriteLine("没有此运算");
+                    return null;
+                    break;
+            }
+        }
+    }
+
+}
--- DesignMode/TestMain.cs
+++ DesignMode/TestMain.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DesignMode.Creational.FactoryPattern;
 
 namespace DesignMode
 {
@@ -7,7 +8,12 @@ namespace DesignMode
         public static void Main(string[] args)
         {
             Console.WriteLine("执行Main");
-            
+            Console.WriteLine( "-----------------------工厂模式--------------------------------");
+            IFactory factory = new Operation().OperationSymbol("+");
+            factory._numberA = 10;
+            factory._numberB = 20;
+            Console.WriteLine( factory.GetResult());
+
         }
     }
 }
